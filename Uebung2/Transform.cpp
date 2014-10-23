7a00433b581aeeebad788249130f8ef73175a166
Stefan B <Stefan.bie@live.de>
2014-10-23 14:49:54 +0200

    - Aufgabe 2 --> Testfälle eingebaut

--- Uebung2/Transform.cpp
+++ Uebung2/Transform.cpp
@@ -21,8 +21,6 @@ Transform::~Transform() {
 }
 array< double , 2> & Transform::Apply(double *x, double *y) {
 
-    cout << *x << " " << *y << endl;
-
     double R[] =    {   cos(this->alpha),-sin(this->alpha),
                         sin(this->alpha),cos(this->alpha)
                     };
@@ -39,8 +37,11 @@ array< double , 2> & Transform::ApplyInverse(double *x, double *y) {
     double R[] =    {   cos(this->alpha),sin(this->alpha),
                         -sin(this->alpha),cos(this->alpha)
                     };
-    double newX = (R[0]**x + R[1]**y) + this->tx;
-    double newY = (R[2]**x + R[3]**y) + this->ty;
+    int localX = (*x-this->tx);
+    int localY = (*y-this->ty);
+
+    double newX = (R[0]*localX + R[1]*localY);
+    double newY = (R[2]*localX + R[3]*localY);
 
     std::array<double , 2> erg{ {newX,newY} };
     return erg;
--- Uebung2/main.cpp
+++ Uebung2/main.cpp
@@ -14,29 +14,36 @@ int main() {
     cout << "Hello, World!" << endl;
 
     long double alpha = M_PI/2;
-    int tx = 3;
-    int ty = 4;
+    double tx = 3;
+    double ty = 4;
     Transform t(alpha,tx,ty);
 
-
-    double dX = 3;
-    double dY = 4;
-
-    double translateVars[2][2][2] = {
+    double translateVars[4][2][2] = {
             {
-                    {3,4},//translate to (x,y)
-                    {7,6},//expected result from Matlab for apply
+                    {4,7},//translate to (x,y)
+                    {-4,8},//expected result from Matlab for apply
 
             },
             {
                     {3,4},//translate to (x,y)
-                    {7,6},//expected result from Matlab for apply
+                    {-1,7},//expected result from Matlab for apply
+
+            },
+            {
+                    {0.5,1.5},//translate to (x,y)
+                    {1.5,4.5},//expected result from Matlab for apply
+
+            },
+            {
+                    {5.0/6.0,1.0/3.0},//translate to (x,y)
+                    {3.0+1.0/(-3.0),5.0+1.0/(-6.0)},//expected result from Matlab for apply
 
             }
 
     };
 
-    int l = sizeof(translateVars);
+    cout.precision(100); // Nachkommerstellen setzten
+    int l = sizeof(translateVars)/sizeof(translateVars[0]); //size of 3d array
     while (l!= 0){
         l--;
         std::array<double ,2> ergApply = t.Apply(&translateVars[l][0][0], &translateVars[l][0][1]);
@@ -46,23 +53,52 @@ int main() {
         /**
         * Apply Inverse Test
         */
-        cout << "apply" << endl;
-        if (ergApply[0] !=  translateVars[l][1][1] || ergApply[1] !=   translateVars[l][1][2]
+        if (ergApply[0] !=  translateVars[l][1][0] || ergApply[1] !=   translateVars[l][1][1]
                 ){
-            cout << l << ". Fehler Apply: \t ";
-            cout << "expeceted: (" <<  translateVars[l][1][1] << "," <<  translateVars[l][1][2] <<") \t" ;
-            cout << "actual:(" << ergApply[0] << "," << ergApply[1] <<")" << endl;
+
+            float ausgangswertX = (float) translateVars[l][1][0];
+            float ausgangswertY = (float) translateVars[l][1][1];
+
+            float ergX = (float) ergApply[0];
+            float ergY = (float) ergApply[1];
+
+            cout << l << ". Fehler Apply: \t\t\t";
+            cout << "expeceted: (" <<   ausgangswertX << "," <<  ausgangswertY <<") \t" ;
+            cout << "actual:(" << ergX << "," <<  ergY <<")" << endl;
         }
         else
-            cout << l <<". OK";
+            cout << l <<". Apply OK" << endl;
 
-        cout << "applyInverse" << endl;
+
+        /**
+        * Apply Inverse
+        */
+        /**
+        * Apply Inverse Test
+        */
+        if (ergApplyInverse[0] !=  translateVars[l][0][0] || ergApplyInverse[1] !=   translateVars[l][0][1]
+                ){
+            std::cout.unsetf ( std::ios::floatfield );
+            cout.precision(15);
+            float ergX = (float) ergApplyInverse[0];
+            float ergY = (float) ergApplyInverse[1];
+
+            float ausgangswertX = (float) translateVars[l][0][0];
+            float ausgangswertY = (float) translateVars[l][0][1];
+
+            cout << l << ". Fehler Apply-Inverse: \t ";
+            cout << "expeceted: (" <<  ausgangswertX << "," <<   ausgangswertY <<") \t" ;
+            cout << "actual:(" << ergX << "," <<  ergY <<")" << endl;
+        }
+        else
+            cout << l <<". Apply OK" << endl;
+
+
     }
 
     /***
     *  Test Fall 1
     >> a = pi/2;
-    >> R = [cos(a), -sin(a);  sin(a),cos(a),];
     >> R = [cos(a), -sin(a);  sin(a),cos(a),];
     >> p = [3;4];
     >> t = [3;4];
